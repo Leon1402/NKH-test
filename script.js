ed690bf9b1b82bde1cfce8228e4f485a8ae5e21e
Никита Каплюхин <nikita-140294@mail.ru>
2021-07-27 16:31:23 +0700

    fix fix header

--- script.js
+++ script.js
@@ -24,13 +24,31 @@ const swiper2 = new Swiper('.hero__swiper', {
     }
 });
 
+
+const swiper3 = new Swiper('.hero__present-swiper', {
+    allowTouchMove: false,
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
+
+
 const header = document.querySelector('.menu')
 
 window.addEventListener('scroll', (e) => {
     if (window.pageYOffset > 300) {
         header.classList.add('menu-fixed')
+        document.querySelector('.main').classList.add('main_active')
     } else {
         header.classList.remove('menu-fixed')
+        document.querySelector('.main').classList.remove('main_active')
     }
 })
 
